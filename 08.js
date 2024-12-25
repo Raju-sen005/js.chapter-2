64eefc5b58bd4de016aee23a3570a9310d23c4ab
Raju_0005 <rajugbp2006@gmail.com>
2024-12-25 14:21:36 +0530

    Add files via upload

---
+++ 08.js
@@ -0,0 +1,45 @@
+// CHAPTER-2 PRACTICE SET
+// P-1
+/*let a = prompt("Enter Your age.")
+age = Number.parseInt(age)
+if(age>10 && age<20){
+  console.log("Your age lies between 10 and 20")
+}
+else{
+  console.log("Your age doesn't lies between 10 and 20")
+}*/
+// P-2
+// let age = prompt("Enter Your age.")
+// switch(age) {
+//   case '12':
+//     console.log("Your age is 12")
+//     break
+//   case '13':
+//     console.log("Your age is 13")
+//     break
+//   case '14':
+//     console.log("Your age is 14")
+//     break
+//   case '15':
+//     console.log("Your age is 15")
+//     break
+//   case 'raju':
+//     console.log("Your age is 16")
+//     break
+//   default:
+//     console.log("Your age is not special.")
+// }
+
+// P-3
+// let num = prompt("Enter a number.")
+// num = Number.parseInt(num)
+// if(num%2 == 0 && num%3 == 0){
+//   console.log("Your number is divisible by 2 and 3")
+// }
+// else{
+//   console.log("Your number is  not divisible by 2 and 3")
+// }
+// P-4
+let age = prompmt("Enter your age.")
+age = age>18?"you Drive":"you can't Drive"
+console.log(age)
